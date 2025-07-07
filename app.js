58b01f20414d74a314ad113209ec1b2b7f25bf2f
Danilo <danilosantossilveira.com@gmail.com>
2025-07-07 11:45:43 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+alert('Bem vindo ao jogo do número secreto');
+let NúmeroSecreto = 8;
+let chute = prompt('Escolha um número de 1 a 30');
+if (chute == NúmeroSecreto) 
+    
+    {console.log ('Parabéns, você acertou')};
---
+++ app2.js
@@ -0,0 +1,13 @@
+let temPao = prompt("Você tem pão em casa? Responda com sim ou não");
+
+if (temPao === "sim") {
+    alert("Fazer sanduíche");
+} else {
+    alert("Fazer miojo");
+}
+
+
+
+
+
+
