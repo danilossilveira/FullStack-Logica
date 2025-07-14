bdf4e6c001a963312176bbe03a5dc5ff28242640
Danilo <danilosantossilveira.com@gmail.com>
2025-07-14 11:48:38 -0300

    Add files via upload

--- app2.js
+++ app2.js
@@ -1,17 +1,14 @@
 alert ("Faça login para acessar o site");
 let tentativasLogin = prompt("Nome de usuário");
 let tentativasSenha = prompt("Senha");
-let login = Danilo;
+let login = "Danilo";
 let senha = "1234";
 
-if (tentativasLogin === login) {
+while ( tentativasLogin != login && tentativasSenha != senha)
+{
+if (tentativasLogin == login && tentativasSenha == senha ) {
     alert (`Acesso liberado, seja bem vindo, ${login}`);
-} else{
-  alert(`Acesso negado, o usuário ${tentativasLogin} não existe`);
+} else {
+  alert("Acesso negado, usuário ou senha incorreto");
 }
-
-if (tentativasSenha === senha ) {
-    alert ("Acesso liberado, seja bem vindo");
-} else{
-  alert(`Acesso negado, o usuário ${tentativasLogin} não existe`);
 }
