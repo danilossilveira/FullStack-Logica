03754194dd66d07991bf08bd26499dfab21142f7
Danilo <danilosantossilveira.com@gmail.com>
2025-07-14 00:48:30 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-alert("Número secreto");
-let numerosecreto = prompt("Escolha um número de 1 a 20");
-if (numerosecreto === "2") {
-    alert("Parabéns, você acertou")
-} else {
-  alert ("Você errou");}
- numerosecreto = prompt("ultima chance")
-if (numerosecreto === "2") {
-    alert("Parabéns, você acertou");
-} else {
-  alert ("Você errou");
-
-
+/*alert ("Jogo do número secreto");
+let numerosecreto = 6;
+console.log (numerosecreto)
+let chute ;
+let tentativas=1 ;
+while (chute != numerosecreto){
+   chute =prompt ("Insira o número secreto");
+if (numerosecreto== chute) {
+ alert (`Você acertou ${numerosecreto} com ${tentativas} tentativas`);
+} else {alert ("Você errou!");
+  if (chute > numerosecreto)
+  {alert (`número secreto é menor que ${chute}`);}
+else { 
+alert (`O número secreto é maior que ${chute}`);
+//Para atribuir mais 1: ++
+tentativas ++;
 }
+}
+
+}*/
+
