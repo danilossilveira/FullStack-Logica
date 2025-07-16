9de12a5bdeb2c96a6fb30ab7c4c830688d985b87
Danilo <danilosantossilveira.com@gmail.com>
2025-07-15 23:45:50 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 alert("Jogo do número secreto");
-let numerosecreto = parseInt( Math.random()*100);
+let dificuldade = 1000;
+let numerosecreto = parseInt( Math.random()* dificuldade +1);
 console.log(numerosecreto);
 let chute;
 let tentativas = 1;
+
 while (chute != numerosecreto) {
-  chute = prompt("Escolha um número de 1 a 100");
+  chute = prompt(`Escolha um número de 1 a  ${dificuldade}`);
   if (numerosecreto == chute) {
     break;
 
   } else {
     alert("Você errou!");
+    tentativas++;
     if (chute > numerosecreto) { alert(`número secreto é menor que ${chute}`); }
     else {
       alert(`O número secreto é maior que ${chute}`);
       //Para atribuir mais 1: ++
-      tentativas++;
     }
   }
 }
-let palavratentativa = tentativas > 1 ? "tentativas" : "Tentativa";
+let palavratentativa = tentativas > 1 ? "Tentativas" : "Tentativa";
 alert(`Você acertou ${numerosecreto} com ${tentativas} ${palavratentativa}`);
